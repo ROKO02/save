3aaaf5d708c976f4b31f4a647287627d62eff420
ROKO02 <90667780+ROKO02@users.noreply.github.com>
2023-05-09 01:19:19 +0900

    Add files via upload

---
+++ pra3/React/day02/ex1/src/App.js
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return "안녕하세요. 리액트 처음입니다.";
+}
+
+export default App;
---
+++ pra3/React/day04/hook/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import State from './components/state';
+import Memo from './components/memo';
+import Effect from './components/effect';
+
+function App() {
+  return (
+  <>
+    <Effect/>
+  </>
+  );
+}
+
+export default App;
